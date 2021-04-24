bf18403f9d1214e9e49d9f8c9667f97f1a94bf19
bdstpierre <barry@stpierre.com>
2021-04-24 02:11:43 -0400

    Partially implementing asking question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,20 @@
 var timeEl = document.querySelector(".timer");
 var question = document.querySelector(".question");
 var answer = document.querySelector(".answer");
-var buttonList = ["Start Quiz"];
+var buttonStart = "Start Quiz";
+var theButton;
+var buttonList = ["1. ", "2. ", "3. ", "4. "];
 var buttonListener = [];
+var secondsLeft = 90;
+var questionNo = 0;
+
+quiz = {
+    question: ["question 1", "question 2"],
+    ans: ["ans 1", "ans 2"],
+    w1: ["w1 1", "w1 2"],
+    w2: ["w2 1", "w2 2"],
+    w3: ["w3 1", "w3 2"],
+};
 
 function init(){
     question.textContent = "Try to answer the following code-related questions within the time limit.  Keep in mind that incorrect answers will penalize your score/time by ten seconds!";
@@ -11,7 +23,7 @@ function init(){
 
     let theButton = document.createElement("button");
     answer.append(theButton);
-    theButton.append(buttonList[0]);
+    theButton.append(buttonStart);
  
     
     theButton.addEventListener("click",function(event){
@@ -25,6 +37,55 @@ function init(){
 
 function startQuiz (){
     console.log("Quiz Started!");
+    //Start timer
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+        }
+    }, 1000);
+
+    askQuestion();
+}
+
+function askQuestion() {
+    if ( questionNo === 0) {
+        answer.textContent = "";
+        for (var i; i < 4; i++){
+        let theButtonListener[i] = document.createElement("button");
+        answer.append(theButtonListener[i]);
+        theButton.append(quiz["ans"][questionNo]);
+     
+        
+        theButtonListener[i].addEventListener("click",function(event){
+            event.preventDefault();
+            console.log(event);
+        
+            // mark wrong or correct
+            // ask next question
+        }
+        
+    
+
+    }
+    
+        question.textContent = quiz["question"][questionNo];
+        // answer.children[0].textContent = quiz["ans"][questionNo];
+        // for(var i = 1; i < 4; i++){
+        //     answer.append(theButton);
+        //     answer.children[i].textContent = quiz["w"+ i][questionNo];
+        // }
+
+
+
+
+}
+
+function endQuiz(){
+    // Things to do when the quiz timer runs out
 }
 
 init();
